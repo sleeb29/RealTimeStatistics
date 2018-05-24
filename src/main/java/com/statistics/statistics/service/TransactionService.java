113228a7cc6ba6596f1b537464c4cab2395c3ec1
sleeb29 <sfleeb@gmail.com>
2018-05-23 23:51:30 -0400

    refactor for multithreading support

--- src/main/java/com/statistics/statistics/service/TransactionService.java
+++ src/main/java/com/statistics/statistics/service/TransactionService.java
@@ -20,18 +20,39 @@ public class TransactionService {
 
     @Value("${statistics.api.window_length_in_milliseconds}")
     private long windowLengthInMilliseconds;
+    private long mostRecentPostTime;
 
-    public synchronized void addTransaction(Transaction transaction, Long currentTime) throws TransactionExpiredException {
+    public void addTransaction(Transaction transaction, Long currentTime) throws TransactionExpiredException {
 
         if(transaction.getTimestamp() < currentTime - windowLengthInMilliseconds){
             throw new TransactionExpiredException(transaction, transaction.getTimestamp());
         }
 
-        StatisticsSnapshot newSnapshot = new StatisticsSnapshot(transaction.getTimestamp(),
-                transaction.getAmount(), 1, transaction.getAmount(), transaction.getAmount(), transaction.getAmount());
-        statisticsSnapshotRepository.addSnapShotEntry(transaction.getTimestamp(), newSnapshot);
+        synchronized(this) {
 
-        refreshRepository(currentTime);
+            //check again with new current time in synchronized block in case thread was
+            //blocked from executing this code for too long and is now actually outside the window
+            long realCurrentTime = System.currentTimeMillis() / 1000L;
+            if(transaction.getTimestamp() < realCurrentTime - windowLengthInMilliseconds){
+                throw new TransactionExpiredException(transaction, transaction.getTimestamp());
+            }
+
+            StatisticsSnapshot newSnapshot = new StatisticsSnapshot(transaction.getTimestamp(),
+                    transaction.getAmount(), 1, transaction.getAmount(), transaction.getAmount(), transaction.getAmount());
+            statisticsSnapshotRepository.addSnapShotEntry(transaction.getTimestamp(), newSnapshot);
+
+            //still want to pass in currentTime to guarantee determinism for invocations of addTransaction
+            //if somehow the most recent post time was updated with a time after the current time use that
+            long timeToUse = currentTime;
+            if(mostRecentPostTime > 0 && mostRecentPostTime > currentTime){
+                timeToUse = mostRecentPostTime;
+            }
+
+            refreshRepository(timeToUse);
+
+            this.mostRecentPostTime = timeToUse;
+
+        }
 
     }
 
@@ -116,6 +137,8 @@ public class TransactionService {
 
     }
 
+    //need to synchronize on the entire instance of the TransactionService object here
+    //as we don't want the repository modified while we retrieve it
     public synchronized StatisticsSnapshot getResult(long endTime){
 
         Map.Entry<Long, StatisticsSnapshot> lastSnapshotEntry = statisticsSnapshotRepository.getLastSnapshotEntry();
