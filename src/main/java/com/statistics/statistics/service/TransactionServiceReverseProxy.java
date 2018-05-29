50ff6630a1de1b4aa72d5c533aa7cbe07e30a109
sleeb29 <sfleeb@gmail.com>
2018-05-28 23:53:27 -0400

    update to use ReadWriteLock for thread safety instead of synchronize keyword

--- src/main/java/com/statistics/statistics/service/TransactionServiceReverseProxy.java
+++ src/main/java/com/statistics/statistics/service/TransactionServiceReverseProxy.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 @Component
 public class TransactionServiceReverseProxy {
 
@@ -16,35 +20,43 @@ public class TransactionServiceReverseProxy {
 
     @Value("${statistics.api.window_length_in_milliseconds}")
     private long windowLengthInMilliseconds;
-    private volatile long mostRecentPostTime;
+    private ReadWriteLock readWriteLock;
+
+    @PostConstruct
+    private void init(){
+        this.readWriteLock = new ReentrantReadWriteLock();
+    }
 
     public void addTransaction(Transaction transaction, Long currentTime) throws TransactionExpiredException, TransactionExpiredDueToServerException {
+
+        Boolean acquiredLock = readWriteLock.writeLock().tryLock();
+        while(!acquiredLock){
+            acquiredLock = readWriteLock.writeLock().tryLock();
+        }
 
         if(transaction.getTimestamp() < currentTime - windowLengthInMilliseconds){
             throw new TransactionExpiredException(transaction, transaction.getTimestamp());
         }
 
-        synchronized(this) {
+        transactionService.addTransaction(transaction, currentTime);
 
-            //check again with new current time in synchronized block in case thread was
-            //blocked from executing this code for too long and is now actually outside the window
-            long realCurrentTime = System.currentTimeMillis() / 1000L;
-            if(transaction.getTimestamp() < realCurrentTime - windowLengthInMilliseconds){
-                throw new TransactionExpiredDueToServerException(transaction, transaction.getTimestamp(), realCurrentTime, currentTime);
-            }
-
-            transactionService.addTransaction(transaction, realCurrentTime);
-
-            this.mostRecentPostTime = realCurrentTime;
-
-        }
+        readWriteLock.writeLock().unlock();
 
     }
 
-    //need to synchronize on the entire instance of the TransactionServiceReverseProxy object here
-    //as we don't want the repository modified while we retrieve it
-    public synchronized StatisticsSnapshot getResult(long endTime) {
-        return transactionService.getResult(endTime);
+    public StatisticsSnapshot getResult(long endTime) {
+
+        Boolean acquiredLock = readWriteLock.readLock().tryLock();
+        while(!acquiredLock){
+            acquiredLock = readWriteLock.readLock().tryLock();
+        }
+
+        StatisticsSnapshot statisticsSnapshot = transactionService.getResult(endTime);
+
+        readWriteLock.readLock().unlock();
+
+        return statisticsSnapshot;
+
     }
 
 }
